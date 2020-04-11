ee22f69f3a5001cfaa29dca87c51be1d0400b33a
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2020-04-11 17:14:13 -0500

    Initial commit

---
+++ db.js
@@ -0,0 +1,20 @@
+var mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+  host: "127.0.0.1", 
+  user: "root", 
+  password: "Password123!",
+  database: "test"
+});
+
+module.exports={
+    getConnection: function(){
+      return new Promise(function(resolve,reject){
+        pool.getConnection().then(function(connection){
+          resolve(connection);
+        }).catch(function(error){
+          reject(error);
+        });
+      });
+    }
+  } 
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const pool = require('./db')
+const app = express()
+const port = 8080
+
+// expose an endpoint "users"
+app.get('/users', async (req, res) => {
+    let conn;
+    try {
+        // establish a connection to MariaDB
+        conn = await pool.getConnection();
+
+        // create a new query
+        var query = "select * from users";
+
+        // execute the query and set the result to a new variable
+        var rows = await conn.query(query);
+
+        // return the results
+        res.send(rows);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.release();
+    }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
